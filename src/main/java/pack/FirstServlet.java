c5ddef2636d9461401710a75f57410178d322568
shev-81 <shev-81@mail.ru>
2022-03-21 12:13:42 +0300

    Дз к 1 лекции Spring

--- src/main/java/pack/FirstServlet.java
+++ src/main/java/pack/FirstServlet.java
@@ -6,46 +6,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-// http://localhost:8080/client2/first_servlet
 @WebServlet(name = "FirstServlet", urlPatterns = "/first")
 public class FirstServlet extends HttpServlet {
     private static Logger logger = LoggerFactory.getLogger(FirstServlet.class);
+    private List<Product> listProducts;
+
+    public FirstServlet() {
+        listProducts = new ArrayList<>();
+        for(int i=0; i < 10; i++){
+            listProducts.add(new Product(i,"Продукт # "+i, (double) i * 2));
+        }
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
         resp.setContentType("text/html;charset=UTF-8");
-        resp.getWriter().printf("<html><body>");
-
+        resp.getWriter().printf("<html>");
         getServletContext().getRequestDispatcher("/header.html").include(req, resp);     // при помощи диспатчера подгружается контент с др ресурса
-        for (int i = 0; i < 10; i++) {
-            resp.getWriter().printf("<h1>Слово" + i + "</h1>");
+        for(Product product: listProducts){
+            resp.getWriter().printf("<div class=\"product\"><div class=\"product_img\"></div> id: "+ product.getId()+ " наименование: "  + product.getTitle()+ " цена: " + product.getPrice() + "</div>");
         }
         getServletContext().getRequestDispatcher("/footer.html").include(req, resp);
-
-        resp.getWriter().printf("</body></html>");
         resp.getWriter().close();
     }
-
-    /*
-    <html><body>
-    <h1>0</h1>
-    <h1>1</h1>
-    <h1>2</h1>
-    <h1>3</h1>
-    <h1>4</h1>
-    <h1>5</h1>
-    <h1>6</h1>
-    <h1>7</h1>
-    <h1>8</h1>
-    <h1>9</h1>
-    </body></html>
-     */
 
     @Override
     public void init() throws ServletException {
